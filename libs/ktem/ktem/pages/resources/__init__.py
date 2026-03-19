b396be54b090cd79243629f907aa8a4d4bfb9f8a
blizZark89 <floriandoeneke@gmail.com>
2026-03-19 09:57:31 +0100

    Always show user management inside resources

--- libs/ktem/ktem/pages/resources/__init__.py
+++ libs/ktem/ktem/pages/resources/__init__.py
@@ -1,12 +1,10 @@
 import gradio as gr
 from ktem.app import BasePage
-from ktem.db.models import User, engine
 from ktem.embeddings.ui import EmbeddingManagement
 from ktem.index.ui import IndexManagement
 from ktem.llms.ui import LLMManagement
 from ktem.mcp.ui import MCPManagement
 from ktem.rerankings.ui import RerankingManagement
-from sqlmodel import Session, select
 
 from .user import UserManagement
 
@@ -33,45 +31,5 @@ class ResourcesTab(BasePage):
             self.mcp_management = MCPManagement(self._app)
 
         if self._app.f_user_management:
-            with gr.Tab("Benutzer", visible=False) as self.user_management_tab:
+            with gr.Tab("Benutzer", visible=True) as self.user_management_tab:
                 self.user_management = UserManagement(self._app)
-
-    def on_subscribe_public_events(self):
-        if self._app.f_user_management:
-            self._app.subscribe_event(
-                name="onSignIn",
-                definition={
-                    "fn": self.toggle_user_management,
-                    "inputs": [self._app.user_id],
-                    "outputs": [self.user_management_tab],
-                    "show_progress": "hidden",
-                },
-            )
-
-            self._app.subscribe_event(
-                name="onSignOut",
-                definition={
-                    "fn": self.toggle_user_management,
-                    "inputs": [self._app.user_id],
-                    "outputs": [self.user_management_tab],
-                    "show_progress": "hidden",
-                },
-            )
-
-    def toggle_user_management(self, user_id):
-        """Show/hide the user management, depending on the user's role"""
-        with Session(engine) as session:
-            user = session.exec(select(User).where(User.id == user_id)).first()
-            if user and user.admin:
-                return gr.update(visible=True)
-
-            return gr.update(visible=False)
-
-    def _on_app_created(self):
-        if self._app.f_user_management:
-            self._app.app.load(
-                self.toggle_user_management,
-                inputs=[self._app.user_id],
-                outputs=[self.user_management_tab],
-                show_progress="hidden",
-            )
