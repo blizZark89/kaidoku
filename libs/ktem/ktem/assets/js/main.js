c6c2f1c66319053958265ba62651c3ea9f64c1c7
blizZark89 <floriandoeneke@gmail.com>
2026-03-27 13:31:39 +0100

    Swap chat settings and language positions

--- libs/ktem/ktem/assets/js/main.js
+++ libs/ktem/ktem/assets/js/main.js
@@ -62,13 +62,13 @@ function run() {
 
   // move use mind-map checkbox
   let mindmap_checkbox = document.getElementById("use-mindmap-checkbox");
-  let citation_dropdown = document.getElementById("citation-dropdown");
+  let language_dropdown = document.getElementById("language-dropdown");
   let chat_setting_panel = document.getElementById("chat-settings-expand");
   chat_setting_panel.insertBefore(
     mindmap_checkbox,
     chat_setting_panel.childNodes[2]
   );
-  chat_setting_panel.insertBefore(citation_dropdown, mindmap_checkbox);
+  chat_setting_panel.insertBefore(language_dropdown, mindmap_checkbox);
 
   // move share conv checkbox
   let report_div = document.querySelector(
--- libs/ktem/ktem/pages/chat/__init__.py
+++ libs/ktem/ktem/pages/chat/__init__.py
@@ -322,7 +322,7 @@ class ChatPage(BasePage):
                 self.chat_panel = ChatPanel(self._app)
 
                 with gr.Accordion(
-                    label="Chat-Einstellungen",
+                    label="Sprache",
                     elem_id="chat-settings-expand",
                     open=False,
                     visible=not KH_DEMO_MODE,
@@ -332,7 +332,7 @@ class ChatPage(BasePage):
                         gr.HTML(
                             "Modell", visible=not KH_DEMO_MODE and not KH_SSO_ENABLED
                         )
-                        gr.HTML("Sprache")
+                        gr.HTML("Chat-Einstellungen")
 
                     with gr.Row():
                         reasoning_setting = (
@@ -367,7 +367,6 @@ class ChatPage(BasePage):
                             container=False,
                             show_label=False,
                             interactive=True,
-                            elem_id="citation-dropdown",
                         )
 
                         self.language = gr.Dropdown(
@@ -375,6 +374,7 @@ class ChatPage(BasePage):
                             value=language_setting.value,
                             container=False,
                             show_label=False,
+                            elem_id="language-dropdown",
                         )
 
                         if not config("USE_LOW_LLM_REQUESTS", default=False, cast=bool):
